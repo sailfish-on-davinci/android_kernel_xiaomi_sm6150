147f7c3aa22daec5a8e5aa9b90e0821650a47a9e
BirdZhang <0312birdzhang@gmail.com>
2026-03-30 20:09:43 +0800

    Add HCI_QUIRK_NO_SUSPEND_NOTIFIER part 2

--- include/net/bluetooth/hci.h
+++ include/net/bluetooth/hci.h
@@ -183,6 +183,14 @@ enum {
 	 * during the hdev->setup vendor callback.
 	 */
 	HCI_QUIRK_NON_PERSISTENT_DIAG,
+
+	/*
+	 * When this quirk is set, then the hci_suspend_notifier is not
+	 * registered. This is intended for devices which drop completely
+	 * from the bus on system-suspend and which will show up as a new
+	 * HCI after resume.
+	 */
+	HCI_QUIRK_NO_SUSPEND_NOTIFIER,
 };
 
 /* HCI device flags */
